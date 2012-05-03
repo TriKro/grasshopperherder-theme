6485339509bc59dd8b7498369554ba3f983480e1
Peter Backx <peter.backx@gmail.com>
2012-05-03 09:05:38 +0200

    removing images from post in columns

--- content.php
+++ content.php
@@ -15,8 +15,8 @@ if($wp_query->current_post == count($posts)-1) {
  ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class($extra_class); ?>>
     <?php if ( has_post_thumbnail() ) : ?>
-        <a href="<?php the_permalink(); ?>">
-        <?php the_post_thumbnail(); ?>
+        <a class="post-thumbnail" href="<?php the_permalink(); ?>">
+        <?php the_post_thumbnail(array(276,176)); ?>
         </a>
     <?php endif; ?> 
 	<h4 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'ghh' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h4>
@@ -34,7 +34,12 @@ if($wp_query->current_post == count($posts)-1) {
 	</div><!-- .entry-summary -->
 	<?php else : ?>
 	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'ghh' ) ); ?>
+		<?php echo preg_replace(
+			"/\[caption .+?\[\/caption\]|\< *[img][^\>]*[.]*\>/i",
+			"",
+			get_the_content(__( 'Continue reading <span class="meta-nav">&rarr;</span>', 'ghh' ))
+			); ?>
+
 		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'ghh' ), 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
 	<?php endif; ?>
