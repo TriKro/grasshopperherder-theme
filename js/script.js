a0ce0af0c78969276474b876f7a2bb2ad54d1f9d
Peter Backx <peter.backx@gmail.com>
2012-05-10 09:31:26 +0200

    javascript solution for underlined images

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-jQuery(document).ready(function(){
-	jQuery('ul.menu > li').hover(
-		function(){
-			if( jQuery(this).find('ul.sub-menu').length > 0 ) {
-				jQuery(this).children('ul.sub-menu').show();
-				jQuery(this).addClass('hovered');
-			}
-		},
-		function(){
-			jQuery(this).children('ul.sub-menu').hide();
-			jQuery(this).removeClass('hovered');
-		}
-	);
+jQuery(document).ready(function(){
+    jQuery(".entry-content p > a:has(img)").addClass("noborder");
+
+	jQuery('ul.menu > li').hover(
+		function(){
+			if( jQuery(this).find('ul.sub-menu').length > 0 ) {
+				jQuery(this).children('ul.sub-menu').show();
+				jQuery(this).addClass('hovered');
+			}
+		},
+		function(){
+			jQuery(this).children('ul.sub-menu').hide();
+			jQuery(this).removeClass('hovered');
+		}
+	);
 });
