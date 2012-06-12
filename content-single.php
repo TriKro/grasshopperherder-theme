fdb8debfe034c35062c83d966f38b942f594b3e4
Tristan Kromer <TK@TristanKromer.com>
2012-06-12 14:18:22 -0700

    Edit footer. Simplify.

--- content-single.php
+++ content-single.php
@@ -25,7 +25,12 @@
 		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'ghh' ), 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
 
+
 	<footer class="entry-meta">
+		Posted on 
+		<time class="entry-meta date">
+			<?php ghh_posted_on_first(); ?>
+		</time>
 		<?php
 			/* translators: used between list items, there is a space after the comma */
 			$category_list = get_the_category_list( __( ', ', 'ghh' ) );
@@ -33,24 +38,7 @@
 			/* translators: used between list items, there is a space after the comma */
 			$tag_list = get_the_tag_list( '', ', ' );
 
-			if ( ! ghh_categorized_blog() ) {
-				// This blog only has 1 category so we just need to worry about tags in the meta text
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'ghh' );
-				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'ghh' );
-				}
-
-			} else {
-				// But this blog has loads of categories so we should probably display them here
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'ghh' );
-				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'ghh' );
-				}
-
-			} // end check for categories on this blog
-
+			$meta_text = __( 'in %1$s &#8226; <a href="%3$s" title="Permalink to %4$s" rel="bookmark">Permalink</a>', 'ghh' );
 			printf(
 				$meta_text,
 				$category_list,
@@ -60,6 +48,7 @@
 			);
 		?>
 
-		<?php edit_post_link( __( 'Edit', 'ghh' ), '<span class="edit-link">', '</span>' ); ?>
+		
+		<?php edit_post_link( __( '&#8226; Edit', 'ghh' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
